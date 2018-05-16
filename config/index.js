dc1b90cb3c381904b34fc716effa12b9ec937e72
dameng <958129909@qq.com>
2018-05-16 19:35:08 +0800

    modify devServer

--- config/index.js
+++ config/index.js
@@ -28,15 +28,15 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      // '/goods':{
-      //     target:'http://localhost:3000'
-      // },
-      // '/goods/*':{
-      //   target:'http://localhost:3000'
-      // },
-      // '/users/*':{
-      //   target:'http://localhost:3000'
-      // }
+      '/goods':{
+          target:'http://localhost:3000'
+      },
+      '/goods/*':{
+        target:'http://localhost:3000'
+      },
+      '/users/*':{
+        target:'http://localhost:3000'
+      }
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
